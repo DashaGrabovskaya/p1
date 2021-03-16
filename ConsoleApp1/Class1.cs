92dbfaa69343b5be0bcacb031658a30e4a11c580
Dasha Grabovskaya <daragrabovskaa9@gmail.com>
2021-03-16 12:52:10 +0300

    4

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -8,5 +8,27 @@ namespace ConsoleApp1
 {
     class Class1
     {
+        public int Number { get; set; }
+        public string Date { get; set; }
+        public decimal Symma { get; set; }
+        public Class1()
+        {
+            Number = 1;
+            Date = null;
+            Symma = 0;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Номер: {0} Год: {1} Сумма: {2}", Number, Date, Symma);
+        }
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите номер: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите дату: ");
+            Date = Console.ReadLine();
+            Console.WriteLine("Введите сумму: ");
+            Symma = Convert.ToDecimal(Console.ReadLine());
+        }
     }
 }
